af346624ec15ea952a3783fee8726b2f9587aedb
shintaroDesu <snkraishin87@gmail.com>
2024-09-16 10:45:03 +0200

    test new update bro

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        Console.WriteLine("Hello worldidddddd");
         CircularArray.Solution solution = new CircularArray.Solution();
         Console.WriteLine(solution.CircularArrayLoop([-1,-2,-3,-4,-5,6]));
     }
