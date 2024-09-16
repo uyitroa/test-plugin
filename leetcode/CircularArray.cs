51df26026e12d308e00563bb8547161a9fb7ae41
shintaroDesu <snkraishin87@gmail.com>
2024-09-16 10:44:47 +0200

    addddddd +4

--- leetcode/CircularArray.cs
+++ leetcode/CircularArray.cs
@@ -5,7 +5,7 @@ namespace CircularArray
         private int next(int cur, ref int[] nums)
         {
             cur = (cur + nums[cur]) % nums.Length;
-            Console.WriteLine("sexting stuff");
+            Console.WriteLine("sexxting stuff");
             cur = cur < 0 ? cur + nums.Length : cur;
             return cur;
         }
